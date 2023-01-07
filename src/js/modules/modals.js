f44aaefa367d49c2183f5dd7b3596f93873d5278
Roman Belykh <106916877+Roman2444@users.noreply.github.com>
2023-01-07 23:28:43 +0300

    addded bindModal to popup_calc_btn, correct gulp

--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -1,7 +1,7 @@
 const modals = () => {
     function bindModal(triggerSelector, modalSelector, closeSelector) {
-        const trigger = document.querySelectorAll(triggerSelector), 
-              modal = document.querySelector(modalSelector), 
+        const trigger = document.querySelectorAll(triggerSelector),
+              modal = document.querySelector(modalSelector),
               close = document.querySelector(closeSelector);
 
         trigger.forEach(item => {
@@ -10,37 +10,38 @@ const modals = () => {
                     e.preventDefault();
                 }
     
-                modal.style.display = 'block';
-                // document.body.classList.add('modal-open')
-                document.body.style.overflow = 'hidden';
-            })
+                modal.style.display = "block";
+                document.body.style.overflow = "hidden";
+                // document.body.classList.add('modal-open');
+            });
         });
 
         close.addEventListener('click', () => {
-            modal.style.display = 'none';
-            // document.body.classList.remove('modal-open')
-            document.body.style.overflow = '';
+            modal.style.display = "none";
+            document.body.style.overflow = "";
+            // document.body.classList.remove('modal-open');
         });
 
         modal.addEventListener('click', (e) => {
             if (e.target === modal) {
-                modal.style.display = 'none';
-                // document.body.classList.remove('modal-open')
-                document.body.style.overflow = '';
+                modal.style.display = "none";
+                document.body.style.overflow = ""; 
+                // document.body.classList.remove('modal-open');
             }
         });
     }
 
     function showModalByTime(selector, time) {
-        setTimeout(() => {
+        setTimeout(function() {
             document.querySelector(selector).style.display = 'block';
-            document.body.style.overflow = 'hidden';
-        }, time)
+            document.body.style.overflow = "hidden";
+        }, time);
     }
-
 
     bindModal('.popup_engineer_btn', '.popup_engineer', '.popup_engineer .popup_close');
     bindModal('.phone_link', '.popup', '.popup .popup_close');
+    bindModal('.popup_calc_btn', '.popup_calc', '.popup_calc_close');
+    
     // showModalByTime('.popup', 60000);
 };
 
