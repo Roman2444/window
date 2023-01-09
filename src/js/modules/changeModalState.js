edb5a0edc9099e1ba75129779b8a6ba8c501c3b8
Roman Belykh <106916877+Roman2444@users.noreply.github.com>
2023-01-09 14:56:25 +0300

    corrected bindActionsToElems to width and height

--- src/js/modules/changeModalState.js
+++ src/js/modules/changeModalState.js
@@ -2,10 +2,10 @@ import checkNumInputs from "./checkNumInputs";
 
 const changeModalState = (state) => {
     const windowForm = document.querySelectorAll('.balcon_icons_img'),
-          windowWith = document.querySelector('#width'),
-          windowHeight = document.querySelector('#height'),
-          indowType = document.querySelector('#view_type'),
-          indowProfile= document.querySelector('.checkbox');
+          windowWith = document.querySelectorAll('#width'),
+          windowHeight = document.querySelectorAll('#height'),
+          indowType = document.querySelectorAll('#view_type'),
+          indowProfile= document.querySelectorAll('.checkbox');
 
     checkNumInputs('#width');
     checkNumInputs('#height');
@@ -13,12 +13,20 @@ const changeModalState = (state) => {
     function bindActionsToElems(event, elem, prop){
         elem.forEach((item, i) => {
             item.addEventListener(event, () => {
-                state[prop] = i;
+                if (elem.length > 1) {
+                    state[prop] = i;
+                } else {
+                    state[prop] = item.value;
+                }
                 console.log(state);
             });
         });
     };
     bindActionsToElems('click', windowForm, 'form');
+    bindActionsToElems('input', windowWith, 'width');
+    bindActionsToElems('input', windowHeight, 'height');
+
+
     // windowForm.forEach((item, i) => {
     //     item.addEventListener('click', () => {
     //         state.form = i;
