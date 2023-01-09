f46578e29a9a63c0cabe3a56e59a165c2b3e126e
Roman Belykh <106916877+Roman2444@users.noreply.github.com>
2023-01-09 13:58:25 +0300

    added variables to changeModalState

--- src/js/modules/changeModalState.js
+++ src/js/modules/changeModalState.js
@@ -1,5 +1,17 @@
 const changeModalState = (state) => {
+    const windowForm = document.querySelectorAll('.balcon_icons_img'),
+          windowWith = document.querySelector('#width'),
+          windowHeight = document.querySelector('#height'),
+          indowType = document.querySelector('#view_type'),
+          indowProfile= document.querySelector('.checkbox');
+    
+    windowForm.forEach((item, i) => {
+        item.addEventListener('click', () => {
+            state.form = i;
+            console.log(state);
+        });
 
+    });
 };
 
 export default changeModalState;
