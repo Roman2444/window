0681d1e5f0de03b6cda30b91587fbc43005fdaad
Roman Belykh <106916877+Roman2444@users.noreply.github.com>
2023-01-10 17:29:42 +0300

    bugs fixed, created images.js

---
+++ src/js/modules/images.js
@@ -0,0 +1,6 @@
+const images = () => {
+    const imgPopup = document.createElement('div'),
+          workSection = document.querySelector('.works'),
+          bigImage = document.createElement('div');
+};
+export default images;
